10546e271ef6b8c4c3f9f0749ef8fb25c75d3249
Dopi <dopi711@googlemail.com>
2010-08-15 14:43:58 +0200

    moved external_alsa-lib, external_alsa-utils and external_iperf-2.0.4 to own repos

--- external/alsa-lib/include/sys.h
+++
@@ -1,6 +0,0 @@
-#warning This header is deprecated, use <alsa/asoundlib.h> instead.
-#ifdef SLSI_S3C6410
-#include <asoundlib.h>
-#else /* SLSI_S3C6410 */
-#include <alsa/asoundlib.h>
-#endif /* SLSI_S3C6410 */
